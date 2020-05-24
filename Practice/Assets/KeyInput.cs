00f158cea3ebe3f10f6feec10e3ffd0b50e0f1b5
Tanner <thebow25@gmail.com>
2020-05-24 15:39:54 -0600

    Getkey with if statements

---
+++ Practice/Assets/KeyInput.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyInput : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	// now this was a tutorial and i could of done getbutton down but its really the same thing for different control schemes 
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		bool down = Input.GetKeyDown(KeyCode.Space);
+		bool held = Input.GetKey(KeyCode.Space);
+		bool up = Input.GetKey(KeyCode.Space);
+
+		if (down)
+		{
+			Debug.Log("Im Down");
+		}
+		else if (held)
+		{
+			Debug.Log("hold it");
+		}
+		else if (up)
+		{
+			Debug.Log("im free");
+		}
+		else
+		{
+			Debug.Log("Norm");
+		}
+		
+		{
+			
+		}
+	}
+}

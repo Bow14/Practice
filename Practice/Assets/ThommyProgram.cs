42499e390622c621b4cb2a6edd90a7249b983e8e
Tanner <thebow25@gmail.com>
2021-08-31 22:04:54 -0600

    Just showing the program coding i messed with idk how to upload to git

---
+++ Practice/Assets/ThommyProgram.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThommyProgram : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start()
+	{
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+	}
+
+/*	# my first_name = input("Enter Stuff:")
+	# my_var = interface (type(my_var)
+	
+	#print(my_var)
+	# print(type(my_var)
+	
+	# Comparison operators 
+	# > < != ==
+	
+	# Boolean operators
+	# and or not
+	
+	# chocolate_cookie = 4.50
+	# sugar_cookie = 4.25
+	# bewl = True
+	
+	print(not(chocolate_cookie > sugar_cookie))*/
+
+	{
+
+	}
+}

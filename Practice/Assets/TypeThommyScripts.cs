056ee42dc1640b92a9c0d05a420979404b58bbf9
Tanner <thebow25@gmail.com>
2021-09-07 23:36:40 -0600

    Added more thommy code

---
+++ Practice/Assets/TypeThommyScripts.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TypeThommyScripts : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+/*
+	
+	'''
+	a = 3
+
+	print(type(a))
+
+	a = float(a)
+		print(type(a))
+
+	a  = str(a)
+	print(type(a))
+
+	a = bool(a)
+		print(type(a))
+
+		'''
+
+	def main():
+
+	name  = "Bianca"
+	food = "cookies"
+	price = 4.5
+	quantity = 3
+
+	print(f"(name.capitalze())'s favortie food is(food), at $(price:2f) a pop")
+	print(f"(quanity) (food) cost a total of $((quantity * price) :.2f)")
+
+	print(f"{name.upper():>15}") #field width = 15; right - justified
+	print(f"{name_lower():^15}") #field width = 15; centered
+	print(f"{name.swapecase():<15}") #field width = 15; left-justified*/
+}

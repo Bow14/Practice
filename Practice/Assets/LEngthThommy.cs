2edd500bf330643e9a22f689ff4e0fce5b687901
Tanner <thebow25@gmail.com>
2021-09-08 21:50:08 -0600

    Testing on how to do bool length strings with thommuy

---
+++ Practice/Assets/LEngthThommy.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LEngthThommy : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+//	if len(string_1) > len(string2):
+//		print("String_1 is longer")
+}

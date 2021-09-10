fb27c2c9449eacc342e1efd9b35448cd03209692
Tanner <thebow25@gmail.com>
2021-09-09 19:58:02 -0600

    Learning on how to call and define functions in python

---
+++ Practice/Assets/DEF.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DEF : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start()
+	{
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+	}
+
+/*
+	
+	'''
+	number = int(input("Enter a character:"))
+
+	print(f'Character: {chr(number)}')
+		'''
+
+	def countdown(num):
+	print(num)
+
+	def main():
+	number = input('Enter a number, 1 - 10:')
+	countdown(number)
+
+		if  name =='main':
+	main()
+*/
+}

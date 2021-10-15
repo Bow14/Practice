08c871cfbd23961b115a825d2e69b8eaf91ab20e
Tanner <thebow25@gmail.com>
2021-10-14 23:29:16 -0600

    Just made a color switcher with ontriggerenter
    been doing alot of programming just need to get back into scripting again

---
+++ Practice/Assets/color.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class color : MonoBehaviour
+{
+
+	[SerializeField] private Material myColor;
+	
+	private void OnTriggerEnter(Collider other)
+	{
+		if (other.CompareTag("Player"))
+		{
+			myColor.color = Color.green;
+		}
+	}
+}
